3d4c59c8c8e9a5aa7c9b9579c57a4cbf71b6660d
Nate <namoricoo@hotmail.com>
2013-10-14 16:45:02 -0700

    increment 4 final

--- madlibs.rb
+++ madlibs.rb
@@ -1,80 +1,51 @@
-#'require pry'
-
 class Madlib
 	
-
-	def prompt
+	def initialize			
 		systemtext,inputNoun,inputAdjective,inputVerb,inputAdverb,inputObject=""
-
 		@madlib= { "Noun" => "", "Adjective" => "","Verb" => "", "Adverb" => "", "Object" => "" }
-
+		puts @arrayOfText
+		@systemArray= ["Noun", "Adjective","Verb","Adverb","Object"]
+		@grammarKeywordArray = ["(noun)", "(adjective)","(verb)","(adverb)","(object)"]
 		
-		systemtext1 = "The (adjective) (noun) suddenly and (adverb) had to (verb) the (object)."
-		systemtext2 = "You should (verb) a (adjective) (noun) to put it ontop of (object)."
-		systemtext3 = "How much is that (noun) in the (object)?"
-
-		arrayOfText=[systemtext1,systemtext2,systemtext3]
-		
-		for i in 0..arrayOfText.length - 1 do
-			if i==0
-				initialInsert(arrayOfText[i])
-			else
-				subsequentInsert(arrayOfText[i])
-			end	
-		end
-		return @madlib	
-	end
-	def subsequentInsert(currentSentence)
-	
-	end	
-	def initialInsert(currentSentence)
-		systemtext=""
-		systemArray= ["Noun", "Adjective","Verb","Adverb","Object"]
-		grammarKeywordArray = ["(noun)", "(adjective)","(verb)","(adverb)","(object)"]						
-		#binding.pry
 		userInputText1="Please enter a noun or noun phrase:"
 		userInputText2="Please enter an Adjective:"
 		userInputText3="Please enter a Verb:"
 		userInputText4="Please enter an Adverb"
 		userInputText5="Please enter an Object"
-				
-		userInputArray =[userInputText1,userInputText2,userInputText3,userInputText4,userInputText5]
-				
-		#grammarArray=[systemtext1, systemtext2, systemtext3]
-				
-		for i in 0..userInputArray.length-1 do
-			systemtext=insert(userInputArray[i],systemArray[i],grammarKeywordArray[i],currentSentence) 
-			currentSentence=systemtext
-		end
-		#puts systemtext		
+		@userInputArray =[userInputText1,userInputText2,userInputText3,userInputText4,userInputText5]
+	end
+
+	def prompt()
+		for i in 0..@userInputArray.length-1 do
+			puts (@userInputArray[i])
+			input = gets.chomp 
+			input = input.strip
+			@madlib[@systemArray[i]] = input	
+		end			
 	end
 	
-	def insert(systemSentence,systemGrammar,grammarKeyword,systemtext)
-		
-		puts systemSentence
-		input = gets.chomp 
-		input = input.strip
-		@madlib[systemGrammar] = input
-		systemtext = systemtext.sub(grammarKeyword, @madlib[systemGrammar])
-		puts systemtext
-		return systemtext
-	end
-	def printHash
-		puts @madlib
-	end
+	def insert(currentSentence)		
+		for i in 0..@userInputArray.length-1 do
+			systemGrammar=@systemArray[i]
+			input=@madlib[systemGrammar];		
+			currentSentence = currentSentence.sub(@grammarKeywordArray[i], @madlib[systemGrammar])		end	
+		puts currentSentence
+		return currentSentence	
+	end	
 	private
 	@madlib
 end
 
+
+systemtext1 = "The (adjective) (noun) suddenly and (adverb) had to (verb) the (object)."
+systemtext2 = "You should (verb) a (adjective) (noun) to put it ontop of (object)."
+systemtext3 = "How much is that (noun) in the (object)?"
+
 madlib=Madlib.new()
-madlib.prompt
-#madlib.printHash
-
-
-
-
-
-
+madlib.prompt()
+madlib.insert(systemtext1)
+madlib.insert(systemtext2)
+madlib.insert(systemtext3)
 
 
 		
